099ad723ebc0a5b887a3e25ebe9e9a83808bd656
patrik <patrik.ellini@outlook.com>
2022-11-04 15:19:59 +0000

    completed the core criteria

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,41 +1,67 @@
+/* eslint-disable array-callback-return */
 class TodoList {
-  constructor () {
+  constructor() {
     this.id = 0
     this.items = []
   }
 
-  create (str) {
+  create(str) {
     this.id++
-    const item = { id: this.id, text: str, status: 'incomplete' }
+    const item = {
+      id: this.id,
+      text: str,
+      status: 'incomplete',
+      date: new Date().toDateString()
+    }
     this.items.push(item)
+
     return item
   }
 
-  showAll () {
+  showAll() {
+    if (this.items.length > 1) {
+      this.items.map((item) => {
+        if (item.text.length > 20) {
+          item.text = item.text.substring(0, 20) + '...'
+        }
+      })
+    }
+
     return this.items
   }
 
-  setComplete (id) {
+  setComplete(id) {
     const item = this.findBy(id)
     item.status = 'complete'
     return item
   }
 
-  getByStatus (status) {
-    return this.items.filter(item => item.status === status)
+  getByStatus(status) {
+    return this.items.filter((item) => item.status === status)
   }
 
-  findBy (id) {
-    const item = this.items.find(item => item.id === id)
+  findBy(id) {
+    const item = this.items.find((item) => item.id === id)
     if (item === undefined) throw new Error('Item not found')
     return item
   }
 
-  deleteBy (id) {
+  filterByDate(date) {
+    console.log('here', new Date().toDateString())
+    const filteredItemes = []
+    this.items.forEach((item) => {
+      if (item.date === date) {
+        filteredItemes.push(item)
+      }
+    })
+    if (filteredItemes === undefined) throw new Error('Item not found')
+    return filteredItemes
+  }
+
+  deleteBy(id) {
     const item = this.findBy(id)
     const index = this.items.indexOf(item)
     return this.items.splice(index, 1)[0]
   }
 }
-
 module.exports = TodoList
